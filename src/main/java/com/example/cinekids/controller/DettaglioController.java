42739c12ca7a9f8e9be5a453af61326d0ed59d21
EmanueleDeCicco97 <emanueledecicco97@gmail.com>
2024-10-24 22:10:31 +0200

    aggiunto a dettaglio il reindirizzamento sul dettaglio attuale dopo le operazioni di login. logout, aggiunta amministratore, aggiunta suggerimento

--- src/main/java/com/example/cinekids/controller/DettaglioController.java
+++ src/main/java/com/example/cinekids/controller/DettaglioController.java
@@ -1,9 +1,11 @@
 package com.example.cinekids.controller;
 
+import com.example.cinekids.model.Admin;
 import com.example.cinekids.model.Film;
 import com.example.cinekids.model.Proiezione;
 import com.example.cinekids.service.FilmService;
 import com.example.cinekids.service.ProiezioneService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -29,10 +28,17 @@ public class DettaglioController {
 
 
     @GetMapping()
-    public String dettaglio(Model model, @RequestParam(name = "id") int idFilm) {
+    public String dettaglio(Model model, @RequestParam(name = "id") int idFilm,
+                                          @RequestParam(name = "error", required = false) String error, HttpSession session,
+                            @RequestParam(name = "errorSuggerimento", required = false) String errorSuggerimento) {
+
+        Admin admin = (Admin) session.getAttribute("admin");
+        model.addAttribute("admin", admin);
+        model.addAttribute("error", error);
+        model.addAttribute("errorSuggerimento", errorSuggerimento);
+
 
         Film film = filmService.dettaglioFilm(idFilm);
-
         Set<LocalDateTime> dateUniche = film.getProiezioni().stream()
                 .map(Proiezione::getDataOra)
                 .collect(Collectors.toSet());
