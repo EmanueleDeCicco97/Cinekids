139a17e475cbfddda35e0e095d08645789073c46
Salvatore Manzi <salmnz7279@gmail.com>
2024-10-25 12:10:52 +0200

    fix pulsante dettagli + modificafilm, aggiungisala

---
+++ src/main/java/com/example/cinekids/controller/AggiungiSalaController.java
@@ -0,0 +1,26 @@
+package com.example.cinekids.controller;
+
+import com.example.cinekids.model.Admin;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/aggiungiSala")
+public class AggiungiSalaController {
+
+    @GetMapping()
+    public String aggiungiSala(Model model, HttpSession session,
+                               @RequestParam(name = "error", required = false) String error,
+                               @RequestParam(name = "errorSuggerimento", required = false) String errorSuggerimento) {
+        Admin admin = (Admin) session.getAttribute("admin");
+        model.addAttribute("admin", admin);
+        model.addAttribute("error", error);
+        model.addAttribute("errorSuggerimento", errorSuggerimento);
+
+        return "aggiungi_sala";
+    }
+}
